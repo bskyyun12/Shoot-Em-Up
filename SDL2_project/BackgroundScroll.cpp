5c66ab8021cfc8991720249b8d23c65c46a6c18b
bskyyun12 <bskyyun12@gmail.com>
2019-11-19 12:33:47 +0100

    Cleaned up background logic

--- SDL2_project/BackgroundScroll.cpp
+++ SDL2_project/BackgroundScroll.cpp
@@ -8,83 +8,46 @@ BackgroundScroll::BackgroundScroll()
 	mTimer = Timer::Instance();
 
 	// Background Variables
-	mBackgroundImageWidth = 500.0f;
-	mBackgroundImageHeight = 500.0f;	
 	mBackgroundStartPos = Vector2D(Graphics::Instance()->SCREEN_WIDTH * 0.5f, Graphics::Instance()->SCREEN_HEIGHT * 0.5f);
 	mBackgroundEndPos = Vector2D(-Graphics::Instance()->SCREEN_WIDTH * 0.5f, Graphics::Instance()->SCREEN_HEIGHT * 0.5f);
 	mScrollSpeed = 0.8f;
 
-	// Background 1 - static
-	mBackground_1 = new Texture("Backgrounds/space/Nebula_Red1.png");
-	mBackground_1->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-
-	// Background 2
-	mBackground_2 = new Texture("Backgrounds/space/Nebula_Red2.png");
-	mBackground_2->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-
-	mBackground_2_1 = new Texture("Backgrounds/space/Nebula_Red2.png");
-	mBackground_2_1->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-	mBackground_2_1->Pos(Vector2D(mBackground_2->Pos().x + Graphics::Instance()->SCREEN_WIDTH, 0.0f));
-
-	mBackground_2_1->Parent(mBackground_2);
-
-	// Background 3
-	mBackground_3 = new Texture("Backgrounds/space/Nebula_Red3.png");
-	mBackground_3->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-
-	mBackground_3_1 = new Texture("Backgrounds/space/Nebula_Red3.png");
-	mBackground_3_1->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-	mBackground_3_1->Pos(Vector2D(mBackground_3->Pos().x + Graphics::Instance()->SCREEN_WIDTH, 0.0f));
-
-	mBackground_3_1->Parent(mBackground_3);
-
-	// Background 4
-	mBackground_4 = new Texture("Backgrounds/space/Nebula_Red4.png");
-	mBackground_4->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-
-	mBackground_4_1 = new Texture("Backgrounds/space/Nebula_Red4.png");
-	mBackground_4_1->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-	mBackground_4_1->Pos(Vector2D(mBackground_4->Pos().x + Graphics::Instance()->SCREEN_WIDTH, 0.0f));
-
-	mBackground_4_1->Parent(mBackground_4);
-
-	// Background 5
-	//mBackground_5 = new Texture("Backgrounds/plx-5.png");
-	//mBackground_5->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-
-	//mBackground_5_1 = new Texture("Backgrounds/plx-5.png");
-	//mBackground_5_1->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mBackgroundImageWidth, (float)Graphics::Instance()->SCREEN_HEIGHT / mBackgroundImageHeight));
-	//mBackground_5_1->Pos(Vector2D(mBackground_5->Pos().x + Graphics::Instance()->SCREEN_WIDTH, 0.0f));
-
-	//mBackground_5_1->Parent(mBackground_5);
-
-	// initialize positions
-	mBackground_1->Pos(mBackgroundStartPos);
-	mBackground_2->Pos(mBackgroundStartPos);
-	mBackground_3->Pos(mBackgroundStartPos);
-	mBackground_4->Pos(mBackgroundStartPos);
-	//mBackground_5->Pos(mBackgroundStartPos);
+	AddBackground("Backgrounds/demon/demon_woods1.png", false);
+	AddBackground("Backgrounds/demon/demon_woods2.png");
+	AddBackground("Backgrounds/demon/demon_woods3.png");
+	AddBackground("Backgrounds/demon/demon_woods4.png");
+	//AddBackground("Backgrounds/5.png");
 }
 
 BackgroundScroll::~BackgroundScroll()
 {
-	//Freeing Background Entities
-	delete mBackground_1;
-	mBackground_1 = nullptr;
-	delete mBackground_2_1;
-	mBackground_2_1 = nullptr;
-	delete mBackground_3;
-	mBackground_3 = nullptr;
-	delete mBackground_3_1;
-	mBackground_3_1 = nullptr;
-	delete mBackground_4;
-	mBackground_4 = nullptr;
-	delete mBackground_4_1;
-	mBackground_4_1 = nullptr;
-	//delete mBackground_5;
-	//mBackground_5 = nullptr;
-	//delete mBackground_5_1;
-	//mBackground_5_1 = nullptr;
+	//teseting
+	delete mTempTex1;
+	mTempTex1 = nullptr;
+	delete mTempTex2;
+	mTempTex2 = nullptr;
+	for (int i = 0; i < mBackgrounds.size(); i++)
+	{
+		delete mBackgrounds[i];
+		mBackgrounds[i] = nullptr;
+	}
+}
+
+void BackgroundScroll::AddBackground(std::string path, bool scroll)
+{
+	mTempTex1 = new Texture(path);
+	mTempTex1->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mTempTex1->ScaledDimensions().x, (float)Graphics::Instance()->SCREEN_HEIGHT / mTempTex1->ScaledDimensions().y));
+	mTempTex1->Pos(mBackgroundStartPos);
+	mBackgrounds.push_back(mTempTex1);
+
+	if (scroll) // make same texture and add it next to first one
+	{
+		mTempTex2 = new Texture(path);
+		mTempTex2->Scale(Vector2D((float)Graphics::Instance()->SCREEN_WIDTH / mTempTex2->ScaledDimensions().x, (float)Graphics::Instance()->SCREEN_HEIGHT / mTempTex2->ScaledDimensions().y));
+		mTempTex2->Pos(Vector2D(mTempTex1->Pos().x + Graphics::Instance()->SCREEN_WIDTH, Graphics::Instance()->SCREEN_HEIGHT * 0.5f));
+		mTempTex2->Parent(mTempTex1);
+		mBackgrounds.push_back(mTempTex2);
+	}
 }
 
 void BackgroundScroll::ResetBackgroundPos(Texture* background)
@@ -97,27 +60,22 @@ void BackgroundScroll::Update()
 {
 	if (mScroll)
 	{
-		mBackground_2->Pos(mBackground_2->Pos() + VECTOR2D_LEFT * mScrollSpeed);
-		mBackground_3->Pos(mBackground_3->Pos() + VECTOR2D_LEFT * mScrollSpeed * 2);
-		mBackground_4->Pos(mBackground_4->Pos() + VECTOR2D_LEFT * mScrollSpeed * 3);
-		//mBackground_5->Pos(mBackground_5->Pos() + VECTOR2D_LEFT * mScrollSpeed * 4);
+		//teseting
+		for (int i = 1; i < mBackgrounds.size(); i += 2)
+		{
+			mBackgrounds[i]->Pos(mBackgrounds[i]->Pos() + VECTOR2D_LEFT * mScrollSpeed * i);
 
-		ResetBackgroundPos(mBackground_2);
-		ResetBackgroundPos(mBackground_3);
-		ResetBackgroundPos(mBackground_4);
-		//ResetBackgroundPos(mBackground_5);
+			if (mBackgrounds[i]->Pos().x < mBackgroundEndPos.x)
+				mBackgrounds[i]->Pos(mBackgroundStartPos);
+		}
 	}
 }
 
 void BackgroundScroll::Render()
 {
-	mBackground_1->Render();
-	mBackground_2->Render();
-	mBackground_2_1->Render();
-	mBackground_3->Render();
-	mBackground_3_1->Render();
-	mBackground_4->Render();
-	mBackground_4_1->Render();
-	//mBackground_5->Render();
-	//mBackground_5_1->Render();
+	//teseting
+	for (int i = 0; i < mBackgrounds.size(); i++)
+	{
+		mBackgrounds[i]->Render();
+	}
 }
